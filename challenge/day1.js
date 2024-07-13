274d57f306ef1d21899af9929201a1591add059b
AishGg <110346378+AishGg@users.noreply.github.com>
2024-07-13 21:31:43 +0000

    day 1

---
+++ challenge/day1.js
@@ -0,0 +1,38 @@
+
+// variable assigning using the var
+var num = 3
+console.log(num);
+
+// variable assigning using the let
+let string = "Ashish"
+console.log(string)
+
+const bool = true
+console.log(bool)
+
+let randomNumber = 4
+let login = false
+let book = {
+    "name": "success",
+    "price": 1
+}
+const numbers = [1, 2, 3, 5, 6]
+
+console.log(typeof (randomNumber))
+console.log(typeof (string))
+console.log(typeof (login))
+console.log(typeof (book))
+console.log(typeof (numbers))
+
+let initial = 5
+console.log(initial)
+initial = 6
+console.log(initial)
+
+const hello = 4
+hello = 5  // const can not be changes or reassigned
+console.log(hello)
+
+
+// there is difference in let and const is that 
+// we can not chnage the value of const but we can do this with let
